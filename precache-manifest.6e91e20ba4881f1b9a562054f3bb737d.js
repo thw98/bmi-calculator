f7d7c2b29256a2df615bb60d46a813c2bfe34634
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-08-19 19:48:09 +0000

    Deploy NXDMN/bmi-calculator to github.com/NXDMN/bmi-calculator.git:gh-pages

---
+++ precache-manifest.6e91e20ba4881f1b9a562054f3bb737d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce9ff86af8557bf9829edab85def0b62",
+    "url": "/bmi-calculator/index.html"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "54b24b2ca3ff34e46ce8",
+    "url": "/bmi-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "b4aa88851593ac880bfa",
+    "url": "/bmi-calculator/static/js/2.0e5f39ad.chunk.js"
+  },
+  {
+    "revision": "54b24b2ca3ff34e46ce8",
+    "url": "/bmi-calculator/static/js/main.17a5ef91.chunk.js"
+  },
+  {
+    "revision": "9b25b4e690523b173542",
+    "url": "/bmi-calculator/static/js/runtime~main.425d477f.js"
+  }
+]);
